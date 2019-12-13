6d0294f8fc185a4307326bba8295d0c642403402
huangzh13 <whuhuangzh@gmail.com>
2019-12-13 22:32:35 +0800

    Building the basic framework.

--- models/Blocks.py
+++ models/Blocks.py
@@ -6,3 +6,17 @@
    Description:  
 -------------------------------------------------
 """
+
+import torch.nn as nn
+
+
+class GSynthesisBlock(nn.Module):
+    def __init__(self):
+        super(GSynthesisBlock, self).__init__()
+
+    def forward(self, x, dlatents_in_range):
+        x = self.conv0_up(x)
+        x = self.epi1(x, dlatents_in_range[:, 0])
+        x = self.conv1(x)
+        x = self.epi2(x, dlatents_in_range[:, 1])
+        return x
--- models/GAN.py
+++ models/GAN.py
@@ -7,4 +7,27 @@
 -------------------------------------------------
 """
 
+import torch.nn as nn
 
+
+class GSynthesis(nn.Module):
+    def __init__(self):
+        super(GSynthesis, self).__init__()
+
+    def forward(self):
+        pass
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+
+class StyleGAN2:
+    def __init__(self):
+        pass
