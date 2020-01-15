67604e0a03ee09a59f96ee0e2b6ac5f6daf028a1
huangzh <whuhuangzh@gmail.com>
2020-01-15 20:35:47 +0800

    Update .gitignore.

--- train.py
+++ train.py
@@ -6,3 +6,24 @@
    Description:  
 -------------------------------------------------
 """
+
+import torch
+
+from models.GAN import Generator
+
+if __name__ == '__main__':
+    # g_synthesis = GSynthesis()
+    # test_dlatents_in = torch.randn(4, 18, 512)
+    # test_imgs_out = g_synthesis(test_dlatents_in)
+
+    # g_mapping = GMapping()
+    # test_latents_in = torch.randn(4, 512)
+    # test_dlatents_out = g_mapping(test_latents_in)
+    device = 'cuda'
+
+    gen = Generator().to(device)
+    test_latents_in = torch.randn(4, 512).to(device)
+    test_imgs_out = gen(test_latents_in)
+
+    print('Done.')
+
